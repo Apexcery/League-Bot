f03390a60fa7de536c576c9a796a47b06cdd2470
Stuart Gleeson <stuart.gleeson98@outlook.com>
2020-10-09 16:36:47 +0100

    Add project files.

---
+++ League-Bot/Commands/Info.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace League_Bot.Commands
+{
+    public class Info : ModuleBase<SocketCommandContext>
+    {
+        [Command("help")]
+        public async Task Help()
+        {
+            var prefix = "!l ";
+
+            var embed = new EmbedBuilder()
+                .WithTitle("Usage")
+                .AddField("Changelog", $"{prefix}changelog\nView the most recent changes to this bot.")
+                .WithColor(Color.Blue)
+                .Build();
+
+            await Context.Channel.SendMessageAsync("", false, embed);
+        }
+    }
+}
---
+++ League-Bot/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using League_Bot.Commands;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace League_Bot
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            var token = @"NzY0MTQ2NTA5OTcyMjQyNDQy.X4CA5Q.3dC6q7HPGJOlOQr52YXeCy5w-2Q";
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+
+            if (message?.Author == null || message.Author.IsBot)
+                return;
+
+            var argPos = 0;
+
+            var prefix = "!l ";
+
+            if (message.HasStringPrefix(prefix, ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess)
+                {
+                    var embed = new EmbedBuilder()
+                        .WithTitle("Usage")
+                        .AddField("Changelog", $"{prefix}changelog\nView the most recent changes to this bot.")
+                        .WithColor(Color.Blue)
+                        .Build();
+
+                    await context.Channel.SendMessageAsync("Unknown Command", false, embed);
+                }
+            }
+        }
+    }
+}
